b4134c1fc7266b6c8f83629265f4fa061a5a2050
Lian Choi <littlecsi27@gmail.com>
2022-03-01 18:26:06 +0000

    init raiden.py

---
+++ raiden/raiden.py
@@ -0,0 +1,13 @@
+"""imports all sub-modules to execute the raiden game.
+
+Leave one blank line.  The rest of this docstring should contain an
+overall description of the module or program.  Optionally, it may also
+contain a brief description of exported classes and functions and/or usage
+examples.
+"""
+
+def main():
+    return None
+
+if __name__ == '__main__':
+    main()
